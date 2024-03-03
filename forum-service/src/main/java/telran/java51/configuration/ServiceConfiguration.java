1b44fae1a3e3651819151654be6b2e5df311900b
Zamotirina <ir.zamotina@gmail.com>
2024-03-03 19:47:45 +0300

    Not final

--- forum-service/src/main/java/telran/java51/configuration/ServiceConfiguration.java
+++ forum-service/src/main/java/telran/java51/configuration/ServiceConfiguration.java
@@ -5,6 +5,8 @@ import org.modelmapper.config.Configuration.AccessLevel;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class ServiceConfiguration {
@@ -17,6 +19,18 @@ public class ServiceConfiguration {
 					.setFieldAccessLevel(AccessLevel.PRIVATE)
 					.setMatchingStrategy(MatchingStrategies.STRICT);
 		return modelMapper;
+	}
+	
+	/*
+	 * 1. Добавляем еще один Bean, то есть помещаем в аппликационный контекст реализацию служебного интерфейса PassportEnCoder для BCrypt
+	 * 
+	 * Теперь по аналогии с ModelMapper мы можем добавить ссылку на PassportEncoder и использовать его
+	 */
+	
+	@Bean
+	public PasswordEncoder getPasswordEncoder() {
+		
+		return new BCryptPasswordEncoder();
 	}
 
 }
